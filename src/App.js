e7aae119287663eae806cc59feb62675f4b877b4
Ankit Yadav <ankityadav943@gmail.com>
2021-08-21 20:27:50 +0530

    routing has been commented out for deployement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");  //enable or disable dark mode
@@ -42,20 +42,22 @@ function App() {
   };
 
   return (
-    <Router>
+  <>
+    {/* <Router> */}
       {/* all components */}
       
         <Navbar title="TextUtils" about="About Us" mode={mode} toggleMode={toggleMode}/>
         <Alert alert={alert} />
-      <Switch>
-        <Route exact path='/'>
+      {/* // <Switch> */}
+        {/* // <Route exact path='/'> */}
           <TextForm heading="Enter the text to Analyze below" mode={mode} showAlert={showAlert} />
-        </Route>
-        <Route exact path='/About'>
-          <About mode={mode} />
-        </Route>
-      </Switch>
-    </Router>
+        {/* // </Route> */}
+        {/* // <Route exact path='/About'>
+        //   <About mode={mode} />
+        // </Route> */}
+      {/* </Switch> */}
+    {/* // </Router> */}
+  </>
   );
 }
 
